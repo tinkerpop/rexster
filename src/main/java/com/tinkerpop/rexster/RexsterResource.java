1858c79a6970d0ff3b4e52ac47a9f27943d21723
spmallette <spmva@genoprime.com>
2010-11-13 14:38:49 -0500

    converting to a more restful manner of dealing with exception
    as a test on the RexsterResource, changed from returning a JSONObject to a javax.ws.rs.core.Response object with explicit server response codes.

--- src/main/java/com/tinkerpop/rexster/RexsterResource.java
+++ src/main/java/com/tinkerpop/rexster/RexsterResource.java
@@ -1,23 +1,40 @@
 package com.tinkerpop.rexster;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+import com.sun.jersey.api.Responses;
+
 @Path("/")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces({MediaType.APPLICATION_JSON })
 public class RexsterResource extends BaseResource {
 
 	@GET
-    public JSONObject evaluate() throws JSONException {
-    	this.resultObject.put("name", "Rexster: A RESTful Graph Shell");
-        this.resultObject.put("graph_count", WebServer.GetRexsterApplication().getGraphCount());
-        this.resultObject.put("query_time", this.sh.stopWatch());
-        this.resultObject.put("up_time", this.getTimeAlive());
-        return this.resultObject;
+    public Response evaluate() {
+		try {
+	    	this.resultObject.put("name", "Rexster: A RESTful Graph Shell");
+	        this.resultObject.put("graph_count", WebServer.GetRexsterApplication().getGraphCount());
+	        this.resultObject.put("query_time", this.sh.stopWatch());
+	        this.resultObject.put("up_time", this.getTimeAlive());
+	        return Response.ok(this.resultObject).build();
+		} catch (JSONException ex) {
+			Map<String, String> m = new HashMap<String, String>();
+			m.put("message", ex.getMessage());
+			JSONObject error = new JSONObject(m);
+			
+			throw new WebApplicationException(
+					Response.status(Status.INTERNAL_SERVER_ERROR).entity(error).build());
+		}
     }
 }
