cf889809f580319bba99899db63f5c31f8ff8cfd
okram <okrammarko@gmail.com>
2010-11-17 11:37:52 -0700

    updated the graph-exammple xml data model to conform to Gremlin directory stucture. In the demo rexster.xml there are now two TinkerGraph graphs loaded graph-example-1 and graph-example-2 so people can play with multiple graphs in parallel.

--- src/test/java/com/tinkerpop/rexster/BaseTest.java
+++ src/test/java/com/tinkerpop/rexster/BaseTest.java
@@ -1,23 +1,16 @@
 package com.tinkerpop.rexster;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
 import junit.framework.TestCase;
-
 import org.apache.commons.configuration.XMLConfiguration;
 import org.codehaus.jettison.json.JSONObject;
 import org.codehaus.jettison.json.JSONTokener;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Properties;
 import java.util.logging.Logger;
-
-import javax.ws.rs.core.EntityTag;
-import javax.ws.rs.core.MediaType;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
@@ -28,22 +21,22 @@ public abstract class BaseTest extends TestCase {
     protected StatisticsHelper sh = new StatisticsHelper();
     private static Logger logger = Logger.getLogger(BaseTest.class.getName());
     public static final String baseURI = "http://localhost:8182/";
-    
+
     public void BaseTest() {
-    	try {
-    		XMLConfiguration properties = new XMLConfiguration();
+        try {
+            XMLConfiguration properties = new XMLConfiguration();
             properties.load(RexsterApplication.class.getResourceAsStream("rexster.xml"));
-    	} catch (Exception e){
-    		e.printStackTrace();
-    	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void startWebServer() throws Exception {
         Thread thread = new Thread() {
             public void run() {
                 try {
-                	XMLConfiguration properties = new XMLConfiguration();
-                    properties.load(RexsterApplication.class.getResourceAsStream("rexster.xml"));                   
+                    XMLConfiguration properties = new XMLConfiguration();
+                    properties.load(RexsterApplication.class.getResourceAsStream("rexster.xml"));
                     webServer = new WebServer(properties, false);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -59,39 +52,39 @@ public abstract class BaseTest extends TestCase {
     }
 
     public static JSONObject getResource(String uri) throws Exception {
-    	Client c = Client.create();
-    	WebResource r = c.resource(uri);
- 
-    	ClientResponse response = r.get(ClientResponse.class);
-    	InputStream stream = response.getEntityInputStream();
-    	
-    	String entity = readToString(stream);
-    	
+        Client c = Client.create();
+        WebResource r = c.resource(uri);
+
+        ClientResponse response = r.get(ClientResponse.class);
+        InputStream stream = response.getEntityInputStream();
+
+        String entity = readToString(stream);
+
         JSONTokener tokener = new JSONTokener(entity);
         return new JSONObject(tokener);
     }
 
     public static JSONObject postResource(String uri) throws Exception {
-    	Client c = Client.create();
-    	WebResource r = c.resource(uri);
- 
-    	ClientResponse response = r.post(ClientResponse.class);
+        Client c = Client.create();
+        WebResource r = c.resource(uri);
+
+        ClientResponse response = r.post(ClientResponse.class);
         InputStream stream = response.getEntityInputStream();
-        
+
         String entity = readToString(stream);
-    	
+
         JSONTokener tokener = new JSONTokener(entity);
         return new JSONObject(tokener);
     }
 
     public static void deleteResource(String uri) throws Exception {
-    	Client c = Client.create();
-    	WebResource r = c.resource(uri);
-    	r.delete();
+        Client c = Client.create();
+        WebResource r = c.resource(uri);
+        r.delete();
     }
-    
-    public static String createURI(String extension){
-    	return createURI("gratefulgraph", extension);
+
+    public static String createURI(String extension) {
+        return createURI("gratefulgraph", extension);
     }
 
     public static String createURI(String graphName, String extension) {
@@ -105,7 +98,7 @@ public abstract class BaseTest extends TestCase {
             logger.info(name + ": " + eventName + " in " + timeInMilliseconds + "ms");
     }
 
-    public static String readToString (InputStream in) throws IOException {
+    public static String readToString(InputStream in) throws IOException {
         StringBuffer out = new StringBuffer();
         byte[] b = new byte[4096];
         for (int n; (n = in.read(b)) != -1;) {
@@ -113,7 +106,7 @@ public abstract class BaseTest extends TestCase {
         }
         return out.toString();
     }
-    
+
     public class ThreadQuery implements Runnable {
         private final String uri;
 
