2e3d29a32a33d42c6147850115522781f673d6ba
okram <okrammarko@gmail.com>
2011-01-26 12:17:44 -0700

    updated GremlinTraversal to work with 0.7

--- src/main/java/com/tinkerpop/rexster/traversals/gremlin/GremlinTraversal.java
+++ src/main/java/com/tinkerpop/rexster/traversals/gremlin/GremlinTraversal.java
@@ -1,7 +1,7 @@
 package com.tinkerpop.rexster.traversals.gremlin;
 
 import com.tinkerpop.blueprints.pgm.Element;
-import com.tinkerpop.blueprints.pgm.Vertex;
+import com.tinkerpop.gremlin.jsr223.GremlinScriptEngine;
 import com.tinkerpop.rexster.Tokens;
 import com.tinkerpop.rexster.traversals.AbstractTraversal;
 import com.tinkerpop.rexster.traversals.ElementJSONObject;
@@ -12,20 +12,21 @@ import org.codehaus.jettison.json.JSONObject;
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Marko A. Rodriguez (http://markorodriguez.com)
  */
 public class GremlinTraversal extends AbstractTraversal {
 
-    private ScriptEngine engine = new ScriptEngineManager().getEngineByName(GREMLIN);
+    private ScriptEngine engine = new GremlinScriptEngine();
 
     private static final String GREMLIN = "gremlin";
-    private static final String ROOT_VARIABLE = "v";
     private static final String GRAPH_VARIABLE = "g";
     private static final String WILDCARD = "*";
-    private static final String ROOT = "root";
     private static final String SCRIPT = "script";
     private static final String RETURN_KEYS = "return_keys";
     protected List<String> returnKeys = null;
@@ -40,29 +41,24 @@ public class GremlinTraversal extends AbstractTraversal {
         try {
             if (this.requestObject.has(SCRIPT)) {
                 String script = this.requestObject.opt(SCRIPT).toString();
-                if (this.requestObject.has(ROOT)) {
-                    Collection<Vertex> roots = getVertices(graph, (JSONObject) this.requestObject.opt(ROOT));
-                    if (null != roots)
-                        engine.getBindings(ScriptContext.ENGINE_SCOPE).put(ROOT_VARIABLE, roots);
-                }
 
                 engine.getBindings(ScriptContext.ENGINE_SCOPE).put(GRAPH_VARIABLE, graph);
 
                 JSONArray results = new JSONArray();
-                for (Object object : (List) engine.eval(script)) {
-                    if (object instanceof Iterable) {
-                        for (Object o : (Iterable) object) {
-                            results.put(prepareOutput(o));
-                        }
-                    } else if (object instanceof Iterator) {
-                        Iterator itty = (Iterator) object;
-                        while (itty.hasNext()) {
-                            results.put(prepareOutput(itty.next()));
-                        }
-                    } else {
-                        results.put(prepareOutput(object));
+                Object result = engine.eval(script);
+                if (result instanceof Iterable) {
+                    for (Object o : (Iterable) result) {
+                        results.put(prepareOutput(o));
                     }
+                } else if (result instanceof Iterator) {
+                    Iterator itty = (Iterator) result;
+                    while (itty.hasNext()) {
+                        results.put(prepareOutput(itty.next()));
+                    }
+                } else {
+                    results.put(prepareOutput(result));
                 }
+
 
                 try {
                     this.resultObject.put(Tokens.RESULTS, results);
@@ -75,10 +71,15 @@ public class GremlinTraversal extends AbstractTraversal {
                 this.success = false;
                 this.message = "no script provided";
             }
-        } catch (Exception e) {
+        }
+
+        catch (Exception e)
+
+        {
             this.success = false;
             this.message = e.getMessage();
         }
+
     }
 
     private Object prepareOutput(Object object) throws JSONException {
@@ -127,7 +128,6 @@ public class GremlinTraversal extends AbstractTraversal {
 
             parameters.put(SCRIPT, "the Gremlin script to be evaluated");
             parameters.put(RETURN_KEYS, "the element property keys to return (default is to return all element properties)");
-            parameters.put(ROOT + ".<key>", "the elements to set $_ to, where <key> is the element property key");
 
             api.put(Tokens.DESCRIPTION, "evaluate an ad-hoc Gremlin script");
             api.put(Tokens.PARAMETERS, parameters);
