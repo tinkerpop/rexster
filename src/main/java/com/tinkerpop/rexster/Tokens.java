57cb391f048b1e4d632dd70d702b627e49c4ac8c
okram <okrammarko@gmail.com>
2011-01-27 09:16:55 -0700

    updated changelog and bumped up to 0.2-SNAPSHOT.

--- src/main/java/com/tinkerpop/rexster/Tokens.java
+++ src/main/java/com/tinkerpop/rexster/Tokens.java
@@ -66,5 +66,5 @@ public class Tokens {
     public static final String TYPE = "type";
     public static final String CLASS = "class";
 
-    public static final String REXSTER_VERSION = "0.1.5";
+    public static final String REXSTER_VERSION = "0.2-SNAPSHOT";
 }
