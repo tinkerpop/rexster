7d7bac33476fc9d1eb0bd6098fb83dd1d4f542a6
stephen mallette <spmallette@gmail.com>
2011-04-19 10:06:05 -0700

    got rexster.bat running on windows with support for extensions.

--- src/main/bin/rexster.bat
+++ src/main/bin/rexster.bat
@@ -4,22 +4,12 @@
 cd ..\lib
 set LIBDIR=%CD%
 
-set CP=
+cd ..\ext
+set EXTDIR=%CD%
 
-for %%i in (*.jar) do call :concatsep %%i
 cd ..\..\..\
 
 set JAVA_OPTIONS=-Xms32M -Xmx512M
 
 :: Launch the application
-java %JAVA_OPTIONS% %JAVA_ARGS% -cp %CP% com.tinkerpop.rexster.WebServer %*
-goto :eof
-
-:concatsep
-if "%CP%" == "" (
-set CP=%LIBDIR%\%1
-)else (
-set CP=%CP%;%LIBDIR%\%1
-)
-
-:skip
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*;%EXTDIR%/*  com.tinkerpop.rexster.WebServer %*
