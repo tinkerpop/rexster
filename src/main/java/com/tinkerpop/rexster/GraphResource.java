5c7b2acb0dc6343bea1e056e256cde236bc87390
stephen mallette <spmva@genoprime.com>
2011-04-28 20:06:06 -0400

    filtered out request for REST request for favicon.ico so they don't show in the logs #81

--- src/main/java/com/tinkerpop/rexster/GraphResource.java
+++ src/main/java/com/tinkerpop/rexster/GraphResource.java
@@ -14,7 +14,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response.Status;
 
-@Path("/{graphname}")
+@Path("/{graphname: (?!favicon.ico).+}")
 public class GraphResource extends AbstractSubResource {
 
     private static Logger logger = Logger.getLogger(GraphResource.class);
