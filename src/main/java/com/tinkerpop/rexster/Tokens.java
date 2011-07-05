de03ceb2dbd365be3020021056be784676046459
stephen mallette <spmva@genoprime.com>
2011-07-05 15:09:39 -0400

    added rexster console...just the basics.

--- src/main/java/com/tinkerpop/rexster/Tokens.java
+++ src/main/java/com/tinkerpop/rexster/Tokens.java
@@ -31,6 +31,10 @@ public class Tokens {
 
     public static final String REXPRO_REXSTER_CONTEXT = "rexster";
 
+    public static final String REXSTER_CONSOLE_HELP = "?h";
+    public static final String REXSTER_CONSOLE_QUIT = "?q";
+    public static final String REXSTER_CONSOLE_BINDINGS = "?b";
+
     public static final String DESCRIPTION = "description";
     public static final String PARAMETERS = "parameters";
     public static final String API = "api";
---
+++ src/main/java/com/tinkerpop/rexster/protocol/RexsterConsole.java
@@ -0,0 +1,136 @@
+package com.tinkerpop.rexster.protocol;
+
+import com.tinkerpop.pipes.SingleIterator;
+import com.tinkerpop.rexster.Tokens;
+import jline.ConsoleReader;
+import jline.History;
+
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Iterator;
+import java.util.Map;
+
+public class RexsterConsole {
+
+    private final RexsterScriptEngine rexster = (RexsterScriptEngine) new RexsterScriptEngineFactory().getScriptEngine();
+    private final PrintStream output = System.out;
+
+    private static final String REXSTER_HISTORY = ".rexster_history";
+
+    public RexsterConsole(String host, int port, String language) throws Exception {
+        this.output.println("\n" + "      //\n" + "     oO ~~-_\n" + "___m(___m___~.___  reXster " + Tokens.VERSION + "\n" + "_|__|__|__|__|__|     [ " + Tokens.REXSTER_CONSOLE_HELP + " = help ]\n");
+
+        this.rexster.put(RexsterScriptEngine.CONFIG_SCOPE_HOST, host);
+        this.rexster.put(RexsterScriptEngine.CONFIG_SCOPE_PORT, port);
+        this.rexster.put(RexsterScriptEngine.CONFIG_SCOPE_LANGUAGE, language);
+
+        this.rexster.setBindings(this.rexster.createBindings(), ScriptContext.ENGINE_SCOPE);
+        this.rexster.setBindings(this.rexster.createBindings(), ScriptContext.GLOBAL_SCOPE);
+
+        this.primaryLoop();
+    }
+
+    public void primaryLoop() throws Exception {
+
+        final ConsoleReader reader = new ConsoleReader();
+        reader.setBellEnabled(false);
+        reader.setUseHistory(true);
+
+        try {
+            History history = new History();
+            history.setHistoryFile(new File(REXSTER_HISTORY));
+            reader.setHistory(history);
+        } catch (IOException e) {
+            System.err.println("Could not find history file");
+        }
+
+        String line = "";
+        this.output.println();
+
+        while (line != null) {
+
+            try {
+                line = "";
+                boolean submit = false;
+                boolean newline = false;
+                while (!submit) {
+                    if (newline)
+                        line = line + "\n" + reader.readLine(RexsterConsole.makeSpace(this.getPrompt().length()));
+                    else
+                        line = line + "\n" + reader.readLine(this.getPrompt());
+                    if (line.endsWith(" .")) {
+                        newline = true;
+                        line = line.substring(0, line.length() - 2);
+                    } else {
+                        line = line.trim();
+                        submit = true;
+                    }
+                }
+
+                if (line.isEmpty())
+                    continue;
+                if (line.equals(Tokens.REXSTER_CONSOLE_QUIT))
+                    return;
+                else if (line.equals(Tokens.REXSTER_CONSOLE_HELP))
+                    this.printHelp();
+                else if (line.equals(Tokens.REXSTER_CONSOLE_BINDINGS))
+                    this.printBindings(this.rexster.getBindings(ScriptContext.ENGINE_SCOPE));
+                else {
+                    Object result = this.rexster.eval(line);
+                    Iterator itty;
+                    if (result instanceof Iterator) {
+                        itty = (Iterator) result;
+                    } else if (result instanceof Iterable) {
+                        itty = ((Iterable) result).iterator();
+                    } else if (result instanceof Map) {
+                        itty = ((Map) result).entrySet().iterator();
+                    } else {
+                        itty = new SingleIterator<Object>(result);
+                    }
+
+                    while (itty.hasNext()) {
+                        this.output.println("==>" + itty.next());
+                    }
+                }
+
+            } catch (Exception e) {
+                this.output.println("Evaluation error: " + e.getMessage());
+            }
+        }
+    }
+
+    public void printHelp() {
+        this.output.println("-= Console Specific =-");
+        this.output.println(Tokens.REXSTER_CONSOLE_QUIT + ": quit");
+    }
+
+    public void printBindings(final Bindings bindings) {
+        for (Map.Entry<String, Object> entry : bindings.entrySet()) {
+            this.output.println(entry);
+        }
+    }
+
+    public String getPrompt() {
+        return "rexster> ";
+    }
+
+    public static String makeSpace(int number) {
+        String space = new String();
+        for (int i = 0; i < number; i++) {
+            space = space + " ";
+        }
+        return space;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String language = args[2];
+
+        new RexsterConsole(host, port, language);
+    }
+}
