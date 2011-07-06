aa430849a711e1e0f8284b77e1d5ab61a29a78cc
stephen mallette <spmva@genoprime.com>
2011-07-06 12:26:34 -0400

    Support Accept-Charset on the request header.

---
+++ src/main/java/com/tinkerpop/rexster/CharsetHolder.java
@@ -0,0 +1,140 @@
+package com.tinkerpop.rexster;
+
+import org.apache.commons.lang.NullArgumentException;
+import org.apache.log4j.Logger;
+
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.*;
+
+public class CharsetHolder implements Comparable<CharsetHolder> {
+    private static Logger logger = Logger.getLogger(CharsetHolder.class);
+
+    private String charset;
+    private float quality = 0.0f;
+    private int order;
+
+    public CharsetHolder(String charset, float quality, int order) {
+
+        if (charset == null) {
+            throw new NullArgumentException("charset");
+        }
+
+        this.charset = charset;
+        this.quality = quality;
+        this.order = order;
+    }
+
+    public String getCharset() {
+        return this.charset;
+    }
+
+    public float getQuality() {
+        return this.quality;
+    }
+
+    public int getOrder() {
+        return this.order;
+    }
+
+    public boolean isSupported() {
+        boolean isSupportedCharset = true;
+        try {
+            isSupportedCharset = Charset.isSupported(this.charset);
+        } catch (IllegalCharsetNameException icne) {
+            isSupportedCharset = false;
+        }
+
+        return isSupportedCharset;
+    }
+
+    public static List<CharsetHolder> getAcceptableCharsets(String acceptCharsetHeaderValue) {
+        ArrayList<CharsetHolder> charsetHolders = new ArrayList<CharsetHolder>();
+
+        String[] charsetStrings = acceptCharsetHeaderValue.split(",");
+        int order = 0;
+
+        CharsetHolder asteriskCharset = null;
+
+        for (String charsetString : charsetStrings) {
+            try {
+                String charsetValue;
+                float qualityValue = 1f;
+
+                String[] charsetComponents = charsetString.split(";");
+
+                charsetValue = charsetComponents[0];
+                if (charsetComponents.length == 2) {
+                    String[] qualityPair = charsetComponents[1].trim().split("=");
+                    if (qualityPair.length == 2) {
+                        qualityValue = Float.parseFloat(qualityPair[1].trim());
+                    }
+                }
+
+                if (charsetValue.equals("*")) {
+                    asteriskCharset = new CharsetHolder("*", qualityValue, order);
+                } else {
+                    charsetHolders.add(new CharsetHolder(charsetValue, qualityValue, order));
+                }
+
+            } catch (Exception ex) {
+                logger.warn("Charset from the request (or rexster.xml) is not valid: [" + charsetString + "].");
+            }
+
+            order++;
+        }
+
+        SortedMap<String, Charset> availableCharsets = Charset.availableCharsets();
+        if (asteriskCharset == null) {
+            for (Map.Entry<String, Charset> availableCharset : availableCharsets.entrySet()) {
+
+                CharsetHolder otherCharsetHolder = new CharsetHolder(availableCharset.getKey(), 0, Integer.MAX_VALUE);;
+                if (availableCharset.getKey().equals("ISO-8859-1")) {
+                    otherCharsetHolder = new CharsetHolder(availableCharset.getKey(), 1, Integer.MAX_VALUE);
+                }
+
+                if (!charsetHolders.contains(otherCharsetHolder)) {
+                    charsetHolders.add(otherCharsetHolder);
+                }
+            }
+        } else {
+            for (Map.Entry<String, Charset> availableCharset : availableCharsets.entrySet()) {
+                CharsetHolder otherCharsetHolder = new CharsetHolder(availableCharset.getKey(), asteriskCharset.getQuality(), 0);
+
+                if (!charsetHolders.contains(otherCharsetHolder)) {
+                    charsetHolders.add(otherCharsetHolder);
+                }
+            }
+        }
+
+        Collections.sort(charsetHolders);
+
+        return charsetHolders;
+    }
+
+    public int compareTo(CharsetHolder charsetHolder) {
+        int compare = this.quality == charsetHolder.getQuality() ? 0 : (this.quality > charsetHolder.getQuality() ? -1 : 1);
+        if (compare == 0) {
+            compare = this.order == charsetHolder.getOrder() ? 0 : (this.order > charsetHolder.getOrder() ? 1 : -1);
+        }
+
+        return compare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CharsetHolder that = (CharsetHolder) o;
+
+        if (!charset.equals(that.charset)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return charset.hashCode();
+    }
+}
---
+++ src/test/java/com/tinkerpop/rexster/CharsetHolderTest.java
@@ -0,0 +1,122 @@
+package com.tinkerpop.rexster;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CharsetHolderTest {
+    @Test
+    public void isSupportedValid() {
+        CharsetHolder charset = new CharsetHolder("UTF-8", 1, 1);
+        Assert.assertTrue(charset.isSupported());
+    }
+
+    @Test
+    public void isSupportedInvalid() {
+        CharsetHolder charset = new CharsetHolder("junk", 1, 1);
+        Assert.assertFalse(charset.isSupported());
+    }
+
+    @Test
+    public void getAcceptableCharsetsSingleNoQuality() {
+        List<CharsetHolder> charsets = CharsetHolder.getAcceptableCharsets("UTF-8");
+
+        // first item will be UTF-8
+        CharsetHolder charset = charsets.get(0);
+
+        Assert.assertEquals("UTF-8", charset.getCharset());
+        Assert.assertEquals(1f, charset.getQuality(), 0);
+        Assert.assertEquals(0, charset.getOrder());
+    }
+
+    @Test
+    public void getAcceptableCharsetsSingleWithQuality() {
+        List<CharsetHolder> charsets = CharsetHolder.getAcceptableCharsets("UTF-8;q=0.5");
+
+        // first item will be ISO-8859-1
+        CharsetHolder charset = charsets.get(0);
+
+        Assert.assertEquals("ISO-8859-1", charset.getCharset());
+        Assert.assertEquals(1f, charset.getQuality(), 0);
+        Assert.assertEquals(Integer.MAX_VALUE, charset.getOrder());
+
+        // check parsing of UTF-8
+        for (CharsetHolder cs : charsets) {
+            if (cs.getCharset().equals("UTF-8")) {
+                charset = cs;
+                break;
+            }
+        }
+
+        Assert.assertEquals("UTF-8", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+        Assert.assertEquals(0, charset.getOrder());
+    }
+
+    @Test
+    public void getAcceptableCharsetsSingleWithQualityAndAsterisk() {
+        List<CharsetHolder> charsets = CharsetHolder.getAcceptableCharsets("UTF-8;q=0.5,*;q=0.1");
+
+        // first item is UTF-8
+        CharsetHolder charset = charsets.get(0);
+
+        Assert.assertEquals("UTF-8", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+        Assert.assertEquals(0, charset.getOrder());
+
+        // everything else should be 0.1 with no more UTF-8 references
+        boolean noUtf8 = true;
+
+        for (CharsetHolder cs : charsets.subList(1, charsets.size())) {
+            if (cs.getCharset().equals("UTF-8")) {
+                noUtf8 = false;
+            }
+
+            Assert.assertEquals(0.1f, cs.getQuality(), 0);
+        }
+
+        Assert.assertTrue(noUtf8);
+    }
+
+    @Test
+    public void getAcceptableCharsetsMultipleCharsetsSameQualityUseOrder() {
+        List<CharsetHolder> charsets = CharsetHolder.getAcceptableCharsets("ISO-8859-1;q=0.1,ISO-8859-2;q=0.5,UTF-8;q=0.5,*;q=0");
+
+        // first item is ISO-8859-2
+        CharsetHolder charset = charsets.get(0);
+        Assert.assertEquals("ISO-8859-2", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+
+        // second item is UTF-8
+        charset = charsets.get(1);
+        Assert.assertEquals("UTF-8", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+
+        // third item is ISO-8859-1
+        charset = charsets.get(2);
+        Assert.assertEquals("ISO-8859-1", charset.getCharset());
+        Assert.assertEquals(0.1f, charset.getQuality(), 0);
+    }
+
+    @Test
+    public void getAcceptableCharsetsMultipleCharsetsNoAsterisk() {
+        List<CharsetHolder> charsets = CharsetHolder.getAcceptableCharsets("ISO-8859-2;q=0.5,UTF-8;q=0.5");
+
+        // third item is ISO-8859-1 - gets a q=1 by default
+        CharsetHolder charset = charsets.get(0);
+        Assert.assertEquals("ISO-8859-1", charset.getCharset());
+        Assert.assertEquals(1f, charset.getQuality(), 0);
+
+        // first item is ISO-8859-2
+        charset = charsets.get(1);
+        Assert.assertEquals("ISO-8859-2", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+
+        // second item is UTF-8
+        charset = charsets.get(2);
+        Assert.assertEquals("UTF-8", charset.getCharset());
+        Assert.assertEquals(0.5f, charset.getQuality(), 0);
+
+    }
+}
