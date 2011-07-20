3648c3edf1e1e952f9630065762b313c2fa4748f
stephen mallette <spmva@genoprime.com>
2011-07-20 13:04:02 -0400

    Add the rexster-console.bat file. #138

---
+++ rexster-console.bat
@@ -0,0 +1,15 @@
+:: Windows launcher script for Rexster Console
+@echo off
+
+cd %CD%\target\
+
+set TARGET=
+
+for /f "tokens=*" %%a in ('dir /b /ad') do (
+if exist "%%a\bin\rexster-console.bat" set TARGET=%%a
+)
+
+cd %TARGET%\bin\
+call rexster-console.bat localhost 8185 gremlin %*
+
+target/rexster-*-standalone/bin/rexster-console.bat localhost 8185 gremlin $@
