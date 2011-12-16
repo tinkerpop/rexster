11504770474f3c53804c6eef8d61e0dd6c8853b5
Stephen Mallette <spmva@genoprime.com>
2011-12-15 21:48:20 -0500

    Added PUT for putting items into indices.
    Separated out the PUT from the POST.  POST is now just for creating new indices.

--- src/main/java/com/tinkerpop/rexster/IndexResource.java
+++ src/main/java/com/tinkerpop/rexster/IndexResource.java
@@ -21,6 +21,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -113,7 +114,8 @@ public class IndexResource extends AbstractSubResource {
     public Response optionsElementsFromIndex() {
         return buildOptionsResponse(HttpMethod.GET.toString(),
                 HttpMethod.DELETE.toString(),
-                HttpMethod.POST.toString());
+                HttpMethod.POST.toString(),
+                HttpMethod.PUT.toString());
     }
 
     /**
@@ -395,12 +397,12 @@ public class IndexResource extends AbstractSubResource {
     @Path("/{indexName}")
     @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response putElementInIndexOrCreateIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, MultivaluedMap<String, String> formParams) {
+    public Response postIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, MultivaluedMap<String, String> formParams) {
         // initializes the request object with the data POSTed to the resource.  URI parameters
         // will then be ignored when the getRequestObject is called as the request object will
         // have already been established.
         this.buildRequestObject(formParams);
-        return this.putElementInIndexOrCreateIndex(graphName, indexName);
+        return this.postIndex(graphName, indexName);
 
     }
 
@@ -416,12 +418,12 @@ public class IndexResource extends AbstractSubResource {
     @Path("/{indexName}")
     @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
     @Consumes({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
-    public Response putElementInIndexOrCreateIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, JSONObject json) {
+    public Response postIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, JSONObject json) {
         // initializes the request object with the data POSTed to the resource.  URI parameters
         // will then be ignored when the getRequestObject is called as the request object will
         // have already been established.
         this.setRequestObject(json);
-        return this.putElementInIndexOrCreateIndex(graphName, indexName);
+        return this.postIndex(graphName, indexName);
     }
 
     /**
@@ -435,29 +437,17 @@ public class IndexResource extends AbstractSubResource {
     @POST
     @Path("/{indexName}")
     @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
-    public Response putElementInIndexOrCreateIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName) {
+    public Response postIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName) {
         final Index index = this.getIndexFromGraph(graphName, indexName);
         final IndexableGraph graph = (IndexableGraph) this.getRexsterApplicationGraph(graphName).getGraph();
 
-        String key = null;
-        Object value = null;
-        String id = null;
         String clazz = null;
         String type = null;
         Set<String> keys = null;
 
         final JSONObject theRequestObject = this.getRequestObject();
 
-        Object temp = theRequestObject.opt(Tokens.KEY);
-        if (null != temp)
-            key = temp.toString();
-        temp = theRequestObject.opt(Tokens.VALUE);
-        if (null != temp)
-            value = ElementHelper.getTypedPropertyValue(temp.toString());
-        temp = theRequestObject.opt(Tokens.ID);
-        if (null != temp)
-            id = temp.toString();
-        temp = theRequestObject.opt(Tokens.CLASS);
+        Object temp = theRequestObject.opt(Tokens.CLASS);
         if (null != temp)
             clazz = temp.toString();
         temp = theRequestObject.opt(Tokens.TYPE);
@@ -486,34 +476,13 @@ public class IndexResource extends AbstractSubResource {
             keys = null;
         }
 
-
-        if (null != index & key != null && value != null && clazz != null && id != null) {
-            try {
-                if (clazz.equals(Tokens.VERTEX))
-                    index.put(key, value, graph.getVertex(id));
-                else if (clazz.equals(Tokens.EDGE))
-                    index.put(key, value, graph.getEdge(id));
-                else {
-                    JSONObject error = generateErrorObject("Index class must be either vertex or edge");
-                    throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(error).build());
-                }
-
-                this.resultObject.put(Tokens.QUERY_TIME, this.sh.stopWatch());
-
-            } catch (JSONException ex) {
-                logger.error(ex);
-
-                JSONObject error = generateErrorObjectJsonFail(ex);
-                throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).build());
-            }
-        } else if (null != index && null != type && null != clazz) {
+        if (null != index && null != type && null != clazz) {
             String msg = "Index [" + indexName + "] on graph [" + graphName + "] already exists";
             logger.info(msg);
 
             JSONObject error = generateErrorObject(msg);
-            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(error).build());
+            throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).build());
         } else if (null == index) {
-
 
             if (null != type && null != clazz) {
                 Index.Type t;
@@ -568,6 +537,98 @@ public class IndexResource extends AbstractSubResource {
             }
         } else {
             String msg = "A key, value, id, and type (vertex/edge) must be provided to add elements to an index";
+            logger.info(msg);
+
+            JSONObject error = generateErrorObject(msg);
+            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(error).build());
+        }
+
+        return Response.ok(this.resultObject).build();
+    }
+
+    /**
+     * PUT http://host/graph/indices/indexName?key=key1&value=value1&class=vertex&id=id1
+     */
+    @PUT
+    @Path("/{indexName}")
+    @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response putElementInIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, MultivaluedMap<String, String> formParams) {
+        // initializes the request object with the data POSTed to the resource.  URI parameters
+        // will then be ignored when the getRequestObject is called as the request object will
+        // have already been established.
+        this.buildRequestObject(formParams);
+        return this.putElementInIndex(graphName, indexName);
+
+    }
+
+    /**
+     * PUT http://host/graph/indices/indexName?key=key1&value=value1&class=vertex&id=id1
+     */
+    @PUT
+    @Path("/{indexName}")
+    @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
+    @Consumes({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
+    public Response putElementInIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName, JSONObject json) {
+        // initializes the request object with the data POSTed to the resource.  URI parameters
+        // will then be ignored when the getRequestObject is called as the request object will
+        // have already been established.
+        this.setRequestObject(json);
+        return this.putElementInIndex(graphName, indexName);
+    }
+
+    /**
+     * PUT http://host/graph/indices/indexName?key=key1&value=value1&class=vertex&id=id1
+     */
+    @PUT
+    @Path("/{indexName}")
+    @Produces({MediaType.APPLICATION_JSON, RexsterMediaType.APPLICATION_REXSTER_JSON, RexsterMediaType.APPLICATION_REXSTER_TYPED_JSON})
+    public Response putElementInIndex(@PathParam("graphname") String graphName, @PathParam("indexName") String indexName) {
+        final Index index = this.getIndexFromGraph(graphName, indexName);
+        final IndexableGraph graph = (IndexableGraph) this.getRexsterApplicationGraph(graphName).getGraph();
+
+        if (index == null) {
+            JSONObject error = generateErrorObject("Index not found.");
+            throw new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(error).build());
+        }
+
+        String key = null;
+        Object value = null;
+        String id = null;
+
+        final JSONObject theRequestObject = this.getRequestObject();
+
+        Object temp = theRequestObject.opt(Tokens.KEY);
+        if (null != temp)
+            key = temp.toString();
+        temp = theRequestObject.opt(Tokens.VALUE);
+        if (null != temp)
+            value = ElementHelper.getTypedPropertyValue(temp.toString());
+        temp = theRequestObject.opt(Tokens.ID);
+        if (null != temp)
+            id = temp.toString();
+
+        if (key != null && value != null && id != null) {
+            try {
+                if (index.getIndexClass().equals(Vertex.class))
+                    index.put(key, value, graph.getVertex(id));
+                else if (index.getIndexClass().equals(Edge.class))
+                    index.put(key, value, graph.getEdge(id));
+                else {
+                    JSONObject error = generateErrorObject("Index class must be either vertex or edge");
+                    throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST).entity(error).build());
+                }
+
+                this.resultObject.put(Tokens.QUERY_TIME, this.sh.stopWatch());
+
+            } catch (JSONException ex) {
+                logger.error(ex);
+
+                JSONObject error = generateErrorObjectJsonFail(ex);
+                throw new WebApplicationException(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).build());
+            }
+        } else {
+            String msg = "A key, value, and id must be provided to add elements to an index";
             logger.info(msg);
 
             JSONObject error = generateErrorObject(msg);
