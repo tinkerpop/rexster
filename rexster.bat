821dffe9d61b7484643c93584fefffd186d34b56
stephen mallette <spmva@genoprime.com>
2012-02-20 08:28:08 -0500

    Fixed broken rexster.bat after mvn restructure.

--- rexster.bat
+++ rexster.bat
@@ -1,15 +1,15 @@
 :: Windows launcher script for Rexster
 @echo off
 
-cd %CD%\target\
+cd %CD%\rexster-server\target\
 
 set TARGET=
 
 for /f "tokens=*" %%a in ('dir /b /ad') do (
-if exist "%%a\rexster-server\bin\rexster.bat" set TARGET=%%a
+if exist "%%a\bin\rexster.bat" set TARGET=%%a
 )
 
 cd %TARGET%\bin\
 call rexster.bat %*
 
-cd ..\..\..
+cd ..\..\..\..
