f8c7ea9c078933e56afac334383f8e39e78c0db1
stephen mallette <spmva@genoprime.com>
2012-05-09 09:45:38 -0400

    Minor refactoring.

--- rexster-server/src/main/java/com/tinkerpop/rexster/protocol/RexProMsgPack.java
+++ rexster-server/src/main/java/com/tinkerpop/rexster/protocol/RexProMsgPack.java
@@ -26,82 +26,85 @@ import static org.msgpack.template.Templates.TValue;
 
 public class RexProMsgPack {
     
+    private static final byte[] emptyBindings;
+    
+    static {{
+        byte [] empty;
+        try {
+            empty = ConsoleScriptResponseMessage.convertBindingsToByteArray(new RexsterBindings());
+        } catch (IOException ioe) {
+            empty = new byte[0];
+        }
+        
+        emptyBindings = empty;
+    }};
+    
     public static void main(String[] args) {
        
         long start = System.currentTimeMillis();
-        
+
         RestHelper.Authentication = new RexsterAuthentication(null, null);
-        /*
-        JSONObject restResult = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/vertices");
+
+        JSONObject restResult = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/tp/gremlin?script=g.V");
         JSONArray restVertices = restResult.optJSONArray(Tokens.RESULTS);
         for (int ix =0; ix < restVertices.length(); ix++)  {
-            JSONObject restVertex = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/vertices/" + restVertices.optJSONObject(ix).optString(Tokens._ID));
+            JSONObject restVertex = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/tp/gremlin?script=g.v(" + restVertices.optJSONObject(ix).optString(Tokens._ID) + ")");
             System.out.println(restVertex);
         }
 
-        restResult = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/edges");
+        restResult = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/tp/gremlin?script=g.E");
         JSONArray restEdges = restResult.optJSONArray(Tokens.RESULTS);
         for (int ix =0; ix < restEdges.length(); ix++)  {
-            JSONObject restEdge = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/edges/" + restEdges.optJSONObject(ix).optString(Tokens._ID));
+            JSONObject restEdge = RestHelper.get("http://localhost:8182/graphs/gratefulgraph/tp/gremlin?script=g.e(" + restEdges.optJSONObject(ix).optString(Tokens._ID) + ")");
             System.out.println(restEdge);
         }
-        */
+
         long checkpoint = System.currentTimeMillis();
         
         try {
             RemoteRexsterSession session = new RemoteRexsterSession("localhost", 8184, 100, "", "", SessionRequestMessage.CHANNEL_MSGPACK);
             session.open();
 
-            ScriptRequestMessage scriptMessage = createScriptRequestMessage(session, "g=rexster.getGraph('gratefulgraph');g.V;");
-            MsgPackScriptResponseMessage resultMessage = (MsgPackScriptResponseMessage) session.sendRequest(scriptMessage, 100);
+            MsgPackScriptResponseMessage resultMessage = (MsgPackScriptResponseMessage) session.sendRequest(
+                    createScriptRequestMessage(session, "g=rexster.getGraph('gratefulgraph');g.V;"), 100);
 
             MessagePack msgpack = new MessagePack();
 
-            ByteArrayInputStream in = new ByteArrayInputStream(resultMessage.Results);
-            Unpacker unpacker = msgpack.createUnpacker(in);
+            Unpacker unpacker = msgpack.createUnpacker(new ByteArrayInputStream(resultMessage.Results));
             
             Iterator<Value> itty = unpacker.iterator();
+            unpacker.close();
             while (itty.hasNext()){
-                Value value = msgpack.read(itty.next().asRawValue().getByteArray());
-                Map<String,Value> map = new Converter(value).read(tMap(TString, TValue));
-                
-                //System.out.println(map);
-                
-                ScriptRequestMessage vertexScriptMessage = createScriptRequestMessage(session, "g.v(" + map.get("id") + ")");
-                MsgPackScriptResponseMessage vertexResultMessage = (MsgPackScriptResponseMessage) session.sendRequest(vertexScriptMessage, 100);
+                Map<String,Value> map = new Converter(msgpack.read(itty.next().asRawValue().getByteArray())).read(tMap(TString, TValue));
 
-                ByteArrayInputStream vertexIn = new ByteArrayInputStream(vertexResultMessage.Results);
-                Unpacker unpackerVertex = msgpack.createUnpacker(vertexIn);
+                MsgPackScriptResponseMessage vertexResultMessage = (MsgPackScriptResponseMessage) session.sendRequest(
+                        createScriptRequestMessage(session, "g.v(" + map.get("id") + ")"), 100);
 
-                Value valueVertex = msgpack.read(unpackerVertex.iterator().next().asRawValue().getByteArray());
-                Map<String,Value> mapVertex = new Converter(valueVertex).read(tMap(TString, TValue));
+                unpacker = msgpack.createUnpacker(new ByteArrayInputStream(vertexResultMessage.Results));
+                Value valueVertex = msgpack.read(unpacker.iterator().next().asRawValue().getByteArray());
+                unpacker.close();
 
-                System.out.println(mapVertex);
+                System.out.println(new Converter(valueVertex).read(tMap(TString, TValue)));
             }
 
-            scriptMessage = createScriptRequestMessage(session, "g.E;");
-            resultMessage = (MsgPackScriptResponseMessage) session.sendRequest(scriptMessage, 100);
+            resultMessage = (MsgPackScriptResponseMessage) session.sendRequest(
+                    createScriptRequestMessage(session, "g.E;"), 100);
 
-            in = new ByteArrayInputStream(resultMessage.Results);
-            unpacker = msgpack.createUnpacker(in);
+            unpacker = msgpack.createUnpacker(new ByteArrayInputStream(resultMessage.Results));
 
             itty = unpacker.iterator();
+            unpacker.close();
             while (itty.hasNext()){
-                Value value = msgpack.read(itty.next().asRawValue().getByteArray());
-                Map<String,Value> map = new Converter(value).read(tMap(TString, TValue));
+                Map<String,Value> map = new Converter(msgpack.read(itty.next().asRawValue().getByteArray())).read(tMap(TString, TValue));
 
-                //System.out.println(map);
+                MsgPackScriptResponseMessage edgeResultMessage = (MsgPackScriptResponseMessage) session.sendRequest(
+                        createScriptRequestMessage(session, "g.e(" + map.get("id") + ")"), 100);
 
-                ScriptRequestMessage edgeScriptMessage = createScriptRequestMessage(session, "g.e(" + map.get("id") + ")");
-                MsgPackScriptResponseMessage edgeResultMessage = (MsgPackScriptResponseMessage) session.sendRequest(edgeScriptMessage, 100);
+                unpacker = msgpack.createUnpacker(new ByteArrayInputStream(edgeResultMessage.Results));
+                Value valueEdge = msgpack.read(unpacker.iterator().next().asRawValue().getByteArray());
+                unpacker.close();
 
-                ByteArrayInputStream edgeIn = new ByteArrayInputStream(edgeResultMessage.Results);
-                Unpacker unpackerEdge = msgpack.createUnpacker(edgeIn);
-
-                Value valueEdge = msgpack.read(unpackerEdge.iterator().next().asRawValue().getByteArray());
-                Map<String,Value> mapEdge = new Converter(valueEdge).read(tMap(TString, TValue));
-
-                System.out.println(mapEdge);
+                System.out.println(new Converter(valueEdge).read(tMap(TString, TValue)));
             }
             
             System.out.println((checkpoint - start) + ":" + (System.currentTimeMillis() - checkpoint));
@@ -114,7 +117,7 @@ public class RexProMsgPack {
         ScriptRequestMessage scriptMessage = new ScriptRequestMessage();
         scriptMessage.setSessionAsUUID(session.getSessionKey());
         scriptMessage.Script = script;
-        scriptMessage.Bindings = ConsoleScriptResponseMessage.convertBindingsToByteArray(new RexsterBindings());
+        scriptMessage.Bindings = emptyBindings;
         scriptMessage.LanguageName = "groovy";
         scriptMessage.Flag = (byte) 0;
         scriptMessage.setRequestAsUUID(UUID.randomUUID());
