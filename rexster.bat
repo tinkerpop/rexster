7b7b718394a6b0d2e19f14729e6e6459ee4e4d1f
stephen mallette <spmva@genoprime.com>
2012-05-17 19:22:37 -0400

    Batch files that work without root data directory/rexster.xml.

--- rexster.bat
+++ rexster.bat
@@ -1,13 +1,5 @@
 :: Windows launcher script for Rexster
 @echo off
 
-cd %CD%\rexster-server\target\
-
-set TARGET=
-
-for /f "tokens=*" %%a in ('dir /b /ad') do (
-if exist "%%a\bin\rexster.bat" set TARGET=%%a
-)
-
-cd %TARGET%\bin\
+cd %CD%\rexster-server
 call rexster.bat %*
