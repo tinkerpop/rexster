dc34ab7104074ade67c0856bdaca52cf237a486d
stephen mallette <spmva@genoprime.com>
2012-06-23 05:53:39 -0400

    Updated bat to put all sub directories in ext on classpath.

--- rexster-server/src/main/bin/rexster.bat
+++ rexster-server/src/main/bin/rexster.bat
@@ -1,15 +1,18 @@
 :: Windows launcher script for Rexster
-@echo off
 
 cd ..\lib
 set LIBDIR=%CD%
 
 cd ..\ext
-set EXTDIR=%CD%
+set EXTDIR=%CD%/*
+
+FOR /D /r %%i in (*) do (
+    set EXTDIR=%EXTDIR%;%%i/*
+)
 
 cd ..\bin
 
 set JAVA_OPTIONS=-Xms32m -Xmx512m
 
 :: Launch the application
-java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*;%EXTDIR%/*  com.tinkerpop.rexster.WebServer %*
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*;%EXTDIR%  com.tinkerpop.rexster.WebServer %*
