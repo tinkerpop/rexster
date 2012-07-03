752ca97b69b53a2cfe33faef63f085d26de76084
stephen mallette <spmva@genoprime.com>
2012-07-03 16:32:51 -0400

    Rexster console working under new distribution model.

--- rexster-console.bat
+++ rexster-console.bat
@@ -1,15 +1,5 @@
 :: Windows launcher script for Rexster Console
 @echo off
 
-cd %CD%\rexster-server\target\
-
-set TARGET=
-
-for /f "tokens=*" %%a in ('dir /b /ad') do (
-if exist "%%a\bin\rexster-console.bat" set TARGET=%%a
-)
-
-cd %TARGET%\bin\
+cd %CD%\rexster-server
 call rexster-console.bat %*
-
-cd ..
