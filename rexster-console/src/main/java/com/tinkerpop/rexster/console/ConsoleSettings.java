7514b258ea90feab233d6334cdf66995e568a4f8
stephen mallette <spmva@genoprime.com>
2012-07-28 13:43:11 -0400

    Factored out settings pulled from the command line.
    Get better separation of concerns around grabbing settings from the console command line.

---
+++ rexster-console/src/main/java/com/tinkerpop/rexster/console/ConsoleSettings.java
@@ -0,0 +1,183 @@
+package com.tinkerpop.rexster.console;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Settings from command line to pass to RexsterConsole instance.
+ *
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class ConsoleSettings {
+
+    private final String DEFAULT_HOST = "127.0.0.1";
+    private final int DEFAULT_PORT = 8184;
+    private final String DEFAULT_LANGUAGE = "groovy";
+    private final int DEFAULT_TIMEOUT = 100;
+
+    private final String host;
+    private final int port;
+    private String language;
+    private final int timeout;
+    private final String username;
+    private final String password;
+    private final String fileToExecute;
+
+    public ConsoleSettings(final String [] commandLineArgs) throws Exception {
+        final CommandLine line = getCliInput(commandLineArgs);
+        this.host = line.getOptionValue("rexsterhost", DEFAULT_HOST);
+
+        final String portString = line.getOptionValue("rexsterport", Integer.toString(DEFAULT_PORT));
+        this.port = parseInt(portString, DEFAULT_PORT);
+
+        if (!Integer.toString(this.port).equals(portString)) {
+            System.out.println("the rexsterport parameter must be an integer value. Defaulting to: [" + port + "]");
+        }
+
+        this.language = line.getOptionValue("language", DEFAULT_LANGUAGE);
+
+        final String timeoutString = line.getOptionValue("timeout", Integer.toString(DEFAULT_TIMEOUT));
+        this.timeout = parseInt(timeoutString, DEFAULT_TIMEOUT);
+
+        if (!Integer.toString(this.timeout).equals(timeoutString)){
+            System.out.println("the timeout parameter must be an integer value. Defaulting to: " + timeout);
+        }
+
+        this.username = line.getOptionValue("user", "");
+        this.password = line.getOptionValue("pass", "");
+        this.fileToExecute = line.getOptionValue("execute", null);
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFileToExecute() {
+        return fileToExecute;
+    }
+
+    public boolean isExecuteMode() {
+        return fileToExecute != null;
+    }
+
+    public String getHostPort() {
+        return "[" + this.host + ":" + this.port + "]";
+    }
+
+    private int parseInt(final String intString, final int intDefault) {
+        try {
+            return Integer.parseInt(intString);
+        } catch (NumberFormatException nfe) {
+            return intDefault;
+        }
+    }
+
+    @SuppressWarnings("static-access")
+    private static Options getCliOptions() {
+        final Option help = new Option("h", "help", false, "print this message");
+
+        final Option hostName = OptionBuilder.withArgName("host-name")
+                .hasArg()
+                .withDescription("the rexster server to connect to")
+                .withLongOpt("rexsterhost")
+                .create("rh");
+
+        final Option port = OptionBuilder.withArgName("port")
+                .hasArg()
+                .withDescription("the port of the rexster server that is serving rexpro")
+                .withLongOpt("rexsterport")
+                .create("rp");
+
+        final Option language = OptionBuilder.withArgName("language")
+                .hasArg()
+                .withDescription("the script engine language to use by default")
+                .withLongOpt("language")
+                .create("l");
+
+        final Option timeout = OptionBuilder.withArgName("seconds")
+                .hasArg()
+                .withDescription("time allowed when waiting for results from server (default 100 seconds)")
+                .withLongOpt("timeout")
+                .create("t");
+
+        final Option scriptFile = OptionBuilder.withArgName("file")
+                .hasArg()
+                .withDescription("script to execute remotely")
+                .withLongOpt("execute")
+                .create("e");
+
+        final Option username = OptionBuilder.withArgName("username")
+                .hasArg()
+                .withDescription("username for authentication (if needed)")
+                .withLongOpt("user")
+                .create("u");
+
+        final Option password = OptionBuilder.withArgName("password")
+                .hasArg()
+                .withDescription("password for authentication (if needed)")
+                .withLongOpt("pass")
+                .create("p");
+
+        final Options options = new Options();
+        options.addOption(help);
+        options.addOption(hostName);
+        options.addOption(port);
+        options.addOption(language);
+        options.addOption(timeout);
+        options.addOption(scriptFile);
+        options.addOption(username);
+        options.addOption(password);
+
+        return options;
+    }
+
+    private static CommandLine getCliInput(final String[] args) throws Exception {
+        final Options options = getCliOptions();
+        final CommandLineParser parser = new GnuParser();
+        final CommandLine line;
+
+        try {
+            line = parser.parse(options, args);
+        } catch (ParseException exp) {
+            throw new Exception("Parsing failed.  Reason: " + exp.getMessage());
+        }
+
+        if (line.hasOption("help")) {
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("rexster console", options);
+            System.exit(0);
+        }
+
+        return line;
+    }
+}
--- rexster-console/src/main/java/com/tinkerpop/rexster/protocol/RexsterConsole.java
+++ rexster-console/src/main/java/com/tinkerpop/rexster/protocol/RexsterConsole.java
@@ -2,6 +2,7 @@ package com.tinkerpop.rexster.protocol;
 
 import com.tinkerpop.pipes.util.iterators.SingleIterator;
 import com.tinkerpop.rexster.Tokens;
+import com.tinkerpop.rexster.console.ConsoleSettings;
 import com.tinkerpop.rexster.protocol.msg.ConsoleScriptResponseMessage;
 import com.tinkerpop.rexster.protocol.msg.ErrorResponseMessage;
 import com.tinkerpop.rexster.protocol.msg.RexProMessage;
@@ -31,63 +32,60 @@ import java.util.UUID;
 public class RexsterConsole {
 
     private RemoteRexsterSession session = null;
-    private String host;
-    private String language;
-    private String username = "";
-    private String password = "";
-    private int port;
-    private int timeout;
     private List<String> currentBindings = new ArrayList<String>();
 
     private final PrintStream output = System.out;
+    private final ConsoleSettings settings;
 
     private static final String REXSTER_HISTORY = ".rexster_history";
 
-    public RexsterConsole(String host, int port, String language, int timeout, String username, String password) throws Exception {
+    public RexsterConsole(final ConsoleSettings settings) {
+        this.settings = settings;
+    }
 
+    public void start() throws Exception {
+        if (this.settings.isExecuteMode()) {
+            oneTimeExecuteScript(readFile(this.settings.getFileToExecute()));
+        } else {
+            this.writeAsciiArt();
+            this.acceptReplCommands();
+        }
+    }
+
+    private void writeAsciiArt() {
         this.output.println("        (l_(l");
         this.output.println("(_______( 0 0");
         this.output.println("(        (-Y-) <woof>");
         this.output.println("l l-----l l");
         this.output.println("l l,,   l l,,");
+    }
 
-        this.host = host;
-        this.port = port;
-        this.language = language;
-        this.timeout = timeout;
-        this.username = username;
-        this.password = password;
-
-        this.output.println("opening session [" + this.host + ":" + this.port + "]");
-        this.session = new RemoteRexsterSession(this.host, this.port, this.timeout, this.username, this.password);
-        this.session.open();
+    private void acceptReplCommands() throws Exception {
+        this.output.println("opening session " + this.settings.getHostPort());
+        this.initAndOpenSessionFromSettings();
 
         if (this.session.isOpen()) {
             this.output.println("?h for help");
-
             this.primaryLoop();
         } else {
             this.output.println("could not connect to the Rexster server");
         }
-
     }
 
-    public RexsterConsole(String host, int port, String language, int timeout, String script, String username, String password) throws Exception {
-        this.host = host;
-        this.port = port;
-        this.language = language;
-        this.timeout = timeout;
-        this.username = username;
-        this.password = password;
-
-        this.session = new RemoteRexsterSession(this.host, this.port, this.timeout, this.username, this.password);
-        this.session.open();
+    private void oneTimeExecuteScript(final String script) {
+        this.initAndOpenSessionFromSettings();
 
         if (!this.session.isOpen()) {
             this.output.println("could not connect to the Rexster server");
         } else {
             this.executeScript(script, false);
         }
+    }
+
+    private void initAndOpenSessionFromSettings() {
+        this.session = new RemoteRexsterSession(this.settings.getHost(), this.settings.getPort(),
+                this.settings.getTimeout(), this.settings.getUsername(), this.settings.getPassword());
+        this.session.open();
     }
 
     public void primaryLoop() throws Exception {
@@ -130,7 +128,7 @@ public class RexsterConsole {
                 if (line.isEmpty())
                     continue;
                 if (line.equals(Tokens.REXSTER_CONSOLE_QUIT)) {
-                    this.output.print("closing session with Rexster [" + this.host + ":" + this.port + "]");
+                    this.output.print("closing session with Rexster " + this.settings.getHostPort());
                     if (this.session != null) {
                         this.session.close();
                         this.session = null;
@@ -142,11 +140,11 @@ public class RexsterConsole {
                 } else if (line.equals(Tokens.REXSTER_CONSOLE_BINDINGS)) {
                     this.printBindings();
                 } else if (line.equals(Tokens.REXSTER_CONSOLE_RESET)) {
-                    this.output.print("resetting session with Rexster [" + this.host + ":" + this.port + "]");
+                    this.output.print("resetting session with Rexster " + this.settings.getHostPort());
                     if (this.session != null) {
                         this.session.reset();
                     } else {
-                        this.session = new RemoteRexsterSession(this.host, this.port, this.timeout, this.username, this.password);
+                        this.initAndOpenSessionFromSettings();
                     }
 
                     // reset binding cache in the console...it will come back fresh from the server on the 
@@ -173,7 +171,7 @@ public class RexsterConsole {
                         this.output.println("specify a language on Rexster ?<language-name>");
                         this.printAvailableLanguages();
                     } else if (this.session.isAvailableLanguage(langToChangeTo)) {
-                        this.language = langToChangeTo;
+                        this.settings.setLanguage(langToChangeTo);
                     } else {
                         this.output.println("not a valid language on Rexster: [" + langToChangeTo + "].");
                         this.printAvailableLanguages();
@@ -193,7 +191,7 @@ public class RexsterConsole {
     }
 
     private void executeScript(String line, boolean showPrefix) {
-        ResultAndBindings result = eval(line, this.language, this.session);
+        ResultAndBindings result = eval(line, this.settings.getLanguage(), this.session);
         Iterator itty;
         if (result.getResult() instanceof Iterator) {
             itty = (Iterator) result.getResult();
@@ -253,7 +251,7 @@ public class RexsterConsole {
     }
 
     public String getPrompt() {
-        return "rexster[" + this.language + "]> ";
+        return "rexster[" + this.settings.getLanguage() + "]> ";
     }
 
     public static String makeSpace(int number) {
@@ -328,142 +326,18 @@ public class RexsterConsole {
         return stringBuilder.toString();
     }
 
-    @SuppressWarnings("static-access")
-    private static Options getCliOptions() {
-        Option help = new Option("h", "help", false, "print this message");
-
-        Option hostName = OptionBuilder.withArgName("host-name")
-                .hasArg()
-                .withDescription("the rexster server to connect to")
-                .withLongOpt("rexsterhost")
-                .create("rh");
-
-        Option port = OptionBuilder.withArgName("port")
-                .hasArg()
-                .withDescription("the port of the rexster server that is serving rexpro")
-                .withLongOpt("rexsterport")
-                .create("rp");
-
-        Option language = OptionBuilder.withArgName("language")
-                .hasArg()
-                .withDescription("the script engine language to use by default")
-                .withLongOpt("language")
-                .create("l");
-
-        Option timeout = OptionBuilder.withArgName("seconds")
-                .hasArg()
-                .withDescription("time allowed when waiting for results from server (default 100 seconds)")
-                .withLongOpt("timeout")
-                .create("t");
-
-        Option scriptFile = OptionBuilder.withArgName("file")
-                .hasArg()
-                .withDescription("script to execute remotely")
-                .withLongOpt("execute")
-                .create("e");
-
-        Option username = OptionBuilder.withArgName("username")
-                .hasArg()
-                .withDescription("username for authentication (if needed)")
-                .withLongOpt("user")
-                .create("u");
-
-        Option password = OptionBuilder.withArgName("password")
-                .hasArg()
-                .withDescription("password for authentication (if needed)")
-                .withLongOpt("pass")
-                .create("p");
-
-        Options options = new Options();
-        options.addOption(help);
-        options.addOption(hostName);
-        options.addOption(port);
-        options.addOption(language);
-        options.addOption(timeout);
-        options.addOption(scriptFile);
-        options.addOption(username);
-        options.addOption(password);
-
-        return options;
-    }
-
-    private static CommandLine getCliInput(final String[] args) throws Exception {
-        Options options = getCliOptions();
-        CommandLineParser parser = new GnuParser();
-        CommandLine line;
-
-        try {
-            line = parser.parse(options, args);
-        } catch (ParseException exp) {
-            throw new Exception("Parsing failed.  Reason: " + exp.getMessage());
-        }
-
-        if (line.hasOption("help")) {
-            HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("rexster console", options);
-            System.exit(0);
-        }
-
-        return line;
-    }
-
     public static void main(String[] args) throws Exception {
-
-        CommandLine line = getCliInput(args);
-
-        String host = "localhost";
-        int port = 8184;
-        String language = "groovy";
-        int timeout = RexPro.DEFAULT_TIMEOUT_SECONDS;
-        String username = "";
-        String password = "";
-
-        if (line.hasOption("rexsterhost")) {
-            host = line.getOptionValue("rexsterhost");
+        try {
+            final ConsoleSettings settings = new ConsoleSettings(args);
+            new RexsterConsole(settings).start();
+        } catch (Exception ex) {
+            die(ex);
         }
+    }
 
-        if (line.hasOption("rexsterport")) {
-            String portString = line.getOptionValue("rexsterport");
-            try {
-                port = Integer.parseInt(portString);
-            } catch (NumberFormatException nfe) {
-                System.out.println("the rexsterport parameter must be an integer value. Defaulting to: [" + port + "]");
-            }
-        }
-
-        if (line.hasOption("language")) {
-            language = line.getOptionValue("language");
-        }
-
-        if (line.hasOption("timeout")) {
-            String timeoutString = line.getOptionValue("timeout");
-            try {
-                port = Integer.parseInt(timeoutString);
-            } catch (NumberFormatException nfe) {
-                System.out.println("the timeout parameter must be an integer value. Defaulting to: " + timeout);
-            }
-        }
-
-        if (line.hasOption("user")) {
-            username = line.getOptionValue("user");
-        }
-
-        if (line.hasOption("pass")) {
-            password = line.getOptionValue("pass");
-        }
-
-        String fileToExecute = null;
-        if (line.hasOption("execute")) {
-            fileToExecute = line.getOptionValue("execute");
-
-            try {
-                new RexsterConsole(host, port, language, timeout, readFile(fileToExecute), username, password);
-            } catch (IOException ioe) {
-                System.out.println("could not read the file specified");
-            }
-
-        } else {
-            new RexsterConsole(host, port, language, timeout, username, password);
-        }
+    private static void die(final Throwable ex) {
+        System.out.println(ex.getMessage() + " (stack trace follows)");
+        ex.printStackTrace();
+        System.exit(1);
     }
 }
