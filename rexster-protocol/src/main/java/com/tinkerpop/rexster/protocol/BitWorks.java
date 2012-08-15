24397754ba1d491cc255f0c314dd96830c0f30d6
Stephen Mallette <spmva@genoprime.com>
2012-08-15 16:41:09 -0400

    Minor refactoring and comments for BitWorks.

--- rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/BitWorks.java
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/BitWorks.java
@@ -18,27 +18,41 @@ import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.UUID;
 
+/**
+ * Helper class for for common byte operations.
+ */
 public class BitWorks {
-    public static byte[] convertUUIDToByteArray(UUID uuid) {
-        ByteBuffer bb = ByteBuffer.allocate(16);
+
+    /**
+     * Converts a UUID to bytes.
+     */
+    public static byte[] convertUUIDToByteArray(final UUID uuid) {
+        final ByteBuffer bb = ByteBuffer.allocate(16);
         bb.putLong(uuid.getMostSignificantBits());
         bb.putLong(uuid.getLeastSignificantBits());
         return bb.array();
     }
 
-    public static UUID convertByteArrayToUUID(byte[] bytes) {
-        ByteBuffer bb = ByteBuffer.wrap(bytes);
-        long mostSignificantBits = bb.getLong();
-        long leastSignificantBits = bb.getLong();
+    /**
+     * Converts a byte array to a UUID.
+     */
+    public static UUID convertByteArrayToUUID(final byte[] bytes) {
+        final ByteBuffer bb = ByteBuffer.wrap(bytes);
+        final long mostSignificantBits = bb.getLong();
+        final long leastSignificantBits = bb.getLong();
 
         return new UUID(mostSignificantBits, leastSignificantBits);
     }
 
-    public static byte[] convertStringsToByteArray(String... values) throws IOException {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    /**
+     * Convert a series of strings to a byte array where each string is prefixed with 4 bytes that
+     * represent the length of the string.
+     */
+    public static byte[] convertStringsToByteArray(final String... values) throws IOException {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
         for (String value : values) {
-            byte[] valueAsBytes = value.getBytes(Charset.forName("UTF-8"));
+            final byte[] valueAsBytes = value.getBytes(Charset.forName("UTF-8"));
             stream.write(ByteBuffer.allocate(4).putInt(valueAsBytes.length).array());
             stream.write(valueAsBytes);
         }
@@ -46,14 +60,14 @@ public class BitWorks {
         return stream.toByteArray();
     }
 
-    public static byte[] convertSerializableBindingsToByteArray(Bindings bindings) throws IOException {
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    public static byte[] convertSerializableBindingsToByteArray(final Bindings bindings) throws IOException {
+        final ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
         for (String key : bindings.keySet()) {
             // don't serialize the rexster key...doesn't make sense to send that back to the client
             // as it is a server side resource
             if (!key.equals(Tokens.REXPRO_REXSTER_CONTEXT)) {
-                Object objectToSerialize = bindings.get(key);
+                final Object objectToSerialize = bindings.get(key);
                 if (objectToSerialize instanceof Serializable
                         && !(objectToSerialize instanceof Graph)
                         && !(objectToSerialize instanceof Edge)
@@ -62,7 +76,7 @@ public class BitWorks {
                     stream.write(ByteBuffer.allocate(4).putInt(key.length()).array());
                     stream.write(key.getBytes());
 
-                    byte[] objectBytes = getFilteredBytesWithLength(objectToSerialize);
+                    final byte[] objectBytes = getFilteredBytesWithLength(objectToSerialize);
                     stream.write(objectBytes);
                 }
             }
@@ -71,24 +85,24 @@ public class BitWorks {
         return stream.toByteArray();
     }
 
-    public static RexsterBindings convertByteArrayToRexsterBindings(byte[] bytes) throws IOException, ClassNotFoundException {
-        ByteBuffer bb = ByteBuffer.wrap(bytes);
-        RexsterBindings bindings = new RexsterBindings();
+    public static RexsterBindings convertByteArrayToRexsterBindings(final byte[] bytes) throws IOException, ClassNotFoundException {
+        final ByteBuffer bb = ByteBuffer.wrap(bytes);
+        final RexsterBindings bindings = new RexsterBindings();
 
         while (bb.hasRemaining()) {
-            int lenOfKeyBinding = bb.getInt();
-            byte[] bindingKeyItem = new byte[lenOfKeyBinding];
+            final int lenOfKeyBinding = bb.getInt();
+            final byte[] bindingKeyItem = new byte[lenOfKeyBinding];
             bb.get(bindingKeyItem);
 
-            String key = new String(bindingKeyItem);
+            final String key = new String(bindingKeyItem);
 
-            int lenOfBinding = bb.getInt();
-            byte[] bindingItem = new byte[lenOfBinding];
+            final int lenOfBinding = bb.getInt();
+            final byte[] bindingItem = new byte[lenOfBinding];
             bb.get(bindingItem);
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bindingItem);
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bindingItem);
+            final ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
 
-            Object o = objectInputStream.readObject();
+            final Object o = objectInputStream.readObject();
 
             bindings.put(key, o);
         }
@@ -96,7 +110,7 @@ public class BitWorks {
         return bindings;
     }
 
-    public static byte[] getFilteredBytesWithLength(Object result) throws IOException {
+    static byte[] getFilteredBytesWithLength(final Object result) throws IOException {
 
         if (result instanceof Serializable
                 && !(result instanceof Graph)
@@ -115,17 +129,17 @@ public class BitWorks {
         }
     }
 
-    public static byte[] getBytesWithLength(Object result) throws IOException {
+    static byte[] getBytesWithLength(final Object result) throws IOException {
         if (result == null) {
             return null;
         }
 
-        ByteArrayOutputStream byteOuputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOuputStream);
+        final ByteArrayOutputStream byteOuputStream = new ByteArrayOutputStream();
+        final ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOuputStream);
         objectOutputStream.writeObject(result);
         objectOutputStream.close();
 
-        ByteBuffer bb = ByteBuffer.allocate(4 + byteOuputStream.size());
+        final ByteBuffer bb = ByteBuffer.allocate(4 + byteOuputStream.size());
         bb.putInt(byteOuputStream.size());
         bb.put(byteOuputStream.toByteArray());
 
