32e1dba8b2dc1d423fa2908999e8d3b3b604944e
Stephen Mallette <spmva@genoprime.com>
2012-08-15 15:40:46 -0400

    Supplied a Jersey Injectable Provider so that Rexster can be embedded.
    Really didn't invent anything new here.  Original work was done by Jordan Lewis to get this injectable provider in play.  I removed that work in the grand refactoring on the way through 2.2.0 as it became less relevant for the standalone version of Rexster.  This puts the class back in so that it fits the current model more cleanly.

---
+++ rexster-server/src/main/java/com/tinkerpop/rexster/server/RexsterApplicationProvider.java
@@ -0,0 +1,128 @@
+package com.tinkerpop.rexster.server;
+
+import com.sun.jersey.api.core.HttpContext;
+import com.sun.jersey.core.spi.component.ComponentContext;
+import com.sun.jersey.core.spi.component.ComponentScope;
+import com.sun.jersey.server.impl.inject.AbstractHttpContextInjectable;
+import com.sun.jersey.spi.inject.Injectable;
+import com.sun.jersey.spi.inject.InjectableProvider;
+import com.tinkerpop.rexster.Tokens;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * A Jersey InjectableProvider and Injectable that supplies Servlets that have a @Context
+ * annotated RexsterApplication field with a DefaultRexsterApplication.
+ * <p/>
+ * Those interested in embedding Rexster into their custom application should write a Provider
+ * class following this pattern that supplies their custom implementation of RexsterApplication.
+ * <p/>
+ * This class allows deployment of Rexster inside of a servlet container like Tomcat.  If
+ * it is deployed in this fashion, rexster.xml need only consist of the graph definitions as
+ * follows:
+ *
+ * <pre>
+ * {@code
+ * <rexster>
+ *     <graphs>
+ *         <graph>
+ *             ...
+ *         </graph>
+ *         ...
+ *     </graphs>
+ * </rexster>
+ * }
+ * </pre>
+ *
+ * The other settings will be ignore and controlled by the servlet container.  While TinkerPop does not
+ * officially support this kind of deployment, it can be done by building Rexster to a WAR with
+ * the appropriate web.xml file which should look something like:
+ *
+ * <pre>
+ * {@code
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <!DOCTYPE web-app PUBLIC "-//Sun Microsystems, Inc.//DTD Web Application 2.3//EN" "http://java.sun.com/dtd/web-app_2_3.dtd">
+ * <web-app xmlns="http://java.sun.com/xml/ns/javaee" version="2.5">
+ *   <display-name>Rexster: A RESTful Graph Shell</display-name>
+ *   <servlet>
+ *     <servlet-name>Jersey Web Application</servlet-name>
+ *     <servlet-class>com.sun.jersey.spi.container.servlet.ServletContainer</servlet-class>
+ *   <init-param>
+ *     <param-name>com.sun.jersey.config.property.resourceConfigClass</param-name>
+ *     <param-value>com.sun.jersey.api.core.PackagesResourceConfig</param-value>
+ *   </init-param>
+ *   <init-param>
+ *     <param-name>com.sun.jersey.config.property.packages</param-name>
+ *     <param-value>com.tinkerpop.rexster;com.tinkerpop.rexster.server</param-value>
+ *   </init-param>
+ *   <init-param>
+ *     <param-name>com.tinkerpop.rexster.config</param-name>
+ *     <param-value>rexster.xml</param-value>
+ *   </init-param>
+ *   </servlet>
+ * </web-app>
+ * }
+ * </pre>
+ *
+ * @author Jordan A. Lewis (http://jordanlewis.org)
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+@Provider
+public class RexsterApplicationProvider extends AbstractHttpContextInjectable<RexsterApplication>
+        implements InjectableProvider<Context, Type> {
+
+    private static RexsterApplication rexster;
+
+    private static XMLConfiguration configurationProperties;
+
+    public RexsterApplicationProvider(@Context ServletContext servletContext, @Context ServletConfig servletConfig) {
+
+        if (rexster == null) {
+            if (configurationProperties == null) {
+                configurationProperties = new XMLConfiguration();
+            }
+
+            final String rexsterXmlFile = servletConfig.getInitParameter("com.tinkerpop.rexster.config");
+
+            try {
+                configurationProperties.load(servletContext.getResourceAsStream(rexsterXmlFile));
+            } catch (Exception e) {
+                throw new RuntimeException("Could not locate " + rexsterXmlFile + " properties file.", e);
+            }
+
+            final List<HierarchicalConfiguration> graphConfigs = configurationProperties.configurationsAt(Tokens.REXSTER_GRAPH_PATH);
+            rexster = new DefaultRexsterApplication(graphConfigs);
+
+        }
+    }
+
+    @Override
+    public RexsterApplication getValue(HttpContext c) {
+        return rexster;
+    }
+
+    @Override
+    public RexsterApplication getValue() {
+        return rexster;
+    }
+
+    @Override
+    public ComponentScope getScope() {
+        return ComponentScope.Singleton;
+    }
+
+    @Override
+    public Injectable getInjectable(ComponentContext ic, Context context, Type type) {
+        if (type.equals(RexsterApplication.class)) {
+            return this;
+        }
+        return null;
+    }
+}
