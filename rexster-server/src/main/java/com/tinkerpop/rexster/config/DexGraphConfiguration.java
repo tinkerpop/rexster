972cac3f4b59a13dd11de63cdaae4eec6ca345b9
Stephen Mallette <spmva@genoprime.com>
2012-10-29 14:34:55 -0400

    Dex configuration has the additonal element for an external configuration file.

--- rexster-server/src/main/java/com/tinkerpop/rexster/config/DexGraphConfiguration.java
+++ rexster-server/src/main/java/com/tinkerpop/rexster/config/DexGraphConfiguration.java
@@ -4,15 +4,39 @@ import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.impls.dex.DexGraph;
 import com.tinkerpop.rexster.Tokens;
 import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.SubnodeConfiguration;
 
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
 public class DexGraphConfiguration implements GraphConfiguration {
+
+    private final static String DEX_CONFIGURATION_PROPERTY = "config-file";
 
     public Graph configureGraphInstance(final Configuration properties) throws GraphConfigurationException {
 
         final String graphFile = properties.getString(Tokens.REXSTER_GRAPH_LOCATION, null);
 
+        if (graphFile == null || graphFile.length() == 0) {
+            throw new GraphConfigurationException("Check graph configuration. Missing or empty configuration element: " + Tokens.REXSTER_GRAPH_LOCATION);
+        }
+
+        // get the <properties> section of the xml configuration
+        final HierarchicalConfiguration graphSectionConfig = (HierarchicalConfiguration) properties;
+        SubnodeConfiguration dexSpecificConfiguration;
+        String dexconfig;
+
         try {
-            return new DexGraph(graphFile);
+            // allow the properties to be optional
+            dexSpecificConfiguration = graphSectionConfig.configurationAt(Tokens.REXSTER_GRAPH_PROPERTIES);
+            dexconfig = dexSpecificConfiguration.getString(DEX_CONFIGURATION_PROPERTY, null);
+        } catch (IllegalArgumentException iae) {
+            dexconfig = null;
+        }
+
+        try {
+            return new DexGraph(graphFile, dexconfig);
         } catch (Exception ex) {
             throw new GraphConfigurationException(ex);
         }
