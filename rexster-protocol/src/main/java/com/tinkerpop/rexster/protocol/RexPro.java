d45f5166c13285b8a048914cb974c6fc6464ea99
Stephen Mallette <spmva@genoprime.com>
2012-11-11 12:39:00 -0500

    RexPro client message transport sends message a bit differently.
    Fixes problem with client not being able to connect to Rexster.  Modified the logging to make things a bit more clear as to what was causing an error.

--- rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/RexPro.java
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/RexPro.java
@@ -15,6 +15,7 @@ import org.glassfish.grizzly.nio.transport.TCPNIOTransport;
 import org.glassfish.grizzly.nio.transport.TCPNIOTransportBuilder;
 
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -26,16 +27,16 @@ public final class RexPro {
 
     public static final int DEFAULT_TIMEOUT_SECONDS = 100;
 
-    public static RexProMessage sendMessage(final String rexProHost, final int rexProPort, final RexProMessage messageToSend) {
+    public static RexProMessage sendMessage(final String rexProHost, final int rexProPort, final RexProMessage messageToSend) throws IOException {
         return sendMessage(rexProHost, rexProPort, messageToSend, DEFAULT_TIMEOUT_SECONDS);
     }
 
     public static RexProMessage sendMessage(final String rexProHost, final int rexProPort,
-                                            final RexProMessage messageToSend, final int timeoutSeconds) {
+                                            final RexProMessage messageToSend, final int timeoutSeconds) throws IOException {
         final FutureImpl<RexProMessage> sessionMessageFuture = SafeFutureImpl.create();
 
         Connection connection = null;
-        final TCPNIOTransport transport = getTransport(sessionMessageFuture);
+        final TCPNIOTransport transport = getTransport(messageToSend, sessionMessageFuture);
 
         try {
             // start transport
@@ -44,25 +45,20 @@ public final class RexPro {
             // Connect client to the server
             final GrizzlyFuture<Connection> future = transport.connect(rexProHost, rexProPort);
             connection = future.get(timeoutSeconds, TimeUnit.SECONDS);
-            connection.write(messageToSend);
             return sessionMessageFuture.get(timeoutSeconds, TimeUnit.SECONDS);
 
         } catch (Exception e) {
-            throw new RuntimeException("Request to Rexster failed [" + rexProHost + ":" + rexProPort + "] - " + e.getMessage(), e);
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-
-                transport.stop();
-            } catch (IOException ioe) {
-                //
+            if (connection == null) {
+                throw new RuntimeException("Can not connect via RexPro - " + e.getMessage(), e);
+            } else {
+                throw new RuntimeException("Request [" + messageToSend.getClass().getName() + "] to Rexster failed [" + rexProHost + ":" + rexProPort + "] - " + e.getMessage(), e);
             }
+        } finally {
+            transport.stop();
         }
     }
 
-    public static TCPNIOTransport getTransport(final FutureImpl<RexProMessage> future) {
+    public static TCPNIOTransport getTransport(final RexProMessage toSend, final FutureImpl<RexProMessage> future) {
         // Create a FilterChain using FilterChainBuilder
         final FilterChainBuilder filterChainBuilder = FilterChainBuilder.stateless();
 
@@ -70,7 +66,7 @@ public final class RexPro {
         // for reading and writing data to the connection
         filterChainBuilder.add(new TransportFilter());
         filterChainBuilder.add(new RexProMessageFilter());
-        filterChainBuilder.add(new CustomClientFilter(future));
+        filterChainBuilder.add(new CustomClientFilter(toSend, future));
 
         // Create TCP NIO transport
         final TCPNIOTransport transport =
@@ -82,17 +78,25 @@ public final class RexPro {
 
     private static final class CustomClientFilter extends BaseFilter {
         private final FutureImpl<RexProMessage> resultFuture;
+        private final RexProMessage toSend;
 
-        public CustomClientFilter(final FutureImpl<RexProMessage> resultFuture) {
+        public CustomClientFilter(final RexProMessage toSend, final FutureImpl<RexProMessage> resultFuture) {
             this.resultFuture = resultFuture;
+            this.toSend = toSend;
+        }
+
+        @Override
+        public NextAction handleConnect(FilterChainContext ctx) throws IOException {
+            ctx.write(this.toSend);
+            return ctx.getStopAction();
         }
 
         @Override
         public NextAction handleRead(FilterChainContext ctx) throws IOException {
             final RexProMessage message = ctx.getMessage();
             resultFuture.result(message);
-
             return ctx.getStopAction();
         }
+
     }
 }
