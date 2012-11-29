630ae107b341f4c2cc6654ba908f3b0729644c56
Stephen Mallette <spmva@genoprime.com>
2012-11-29 09:54:00 -0500

    Allow softlinks for extensions per #257

--- rexster-server/src/main/bin/rexster.sh
+++ rexster-server/src/main/bin/rexster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /:/g')
-CP=$CP:$(find `dirname $0`/../ext/ -name "*.jar" | tr '\n' ':')
+CP=$CP:$(find -L `dirname $0`/../ext/ -name "*.jar" | tr '\n' ':')
 #echo $CP
 
 PUBLIC=`dirname $0`/../public/
