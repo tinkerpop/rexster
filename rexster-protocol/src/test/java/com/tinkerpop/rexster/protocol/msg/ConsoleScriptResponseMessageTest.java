892b25ef8235230e979ded74a8fb266f1c604c40
Blake Eggleston <bdeggleston@gmail.com>
2013-02-08 09:49:28 -0800

    adding tests around message serialization and meta validation

--- rexster-protocol/src/test/java/com/tinkerpop/rexster/protocol/msg/ConsoleScriptResponseMessageTest.java
+++ rexster-protocol/src/test/java/com/tinkerpop/rexster/protocol/msg/ConsoleScriptResponseMessageTest.java
@@ -2,11 +2,18 @@ package com.tinkerpop.rexster.protocol.msg;
 
 import junit.framework.Assert;
 import org.junit.Test;
+import org.msgpack.MessagePack;
+import org.msgpack.packer.Packer;
+import org.msgpack.unpacker.Unpacker;
 
 import javax.script.Bindings;
 import javax.script.SimpleBindings;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
@@ -55,5 +62,47 @@ public class ConsoleScriptResponseMessageTest {
         msg.Bindings = ConsoleScriptResponseMessage.convertBindingsToConsoleLineByteArray(b);
 
         Assert.assertEquals(64, msg.estimateMessageSize());
+    }
+
+    @Test
+    public void testSerialization() {
+        MessagePack msgpack = new MessagePack();
+        msgpack.register(RexProMessageMeta.class, RexProMessageMeta.SerializationTemplate.getInstance());
+
+        ConsoleScriptResponseMessage outMsg = new ConsoleScriptResponseMessage();
+        outMsg.Version = 0;
+        outMsg.setRequestAsUUID(UUID.randomUUID());
+        outMsg.setSessionAsUUID(UUID.randomUUID());
+        outMsg.ConsoleLines = new String[2];
+        outMsg.ConsoleLines[0] = "a";
+        outMsg.ConsoleLines[1] = "b";
+        outMsg.Bindings = new byte[1];
+        outMsg.Bindings[0] = 1;
+
+        final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        final Packer packer = msgpack.createPacker(outStream);
+        try {
+            packer.write(outMsg);
+            packer.close();
+        } catch (IOException ex) {
+            Assert.fail();
+        }
+
+        byte[] bytes = outStream.toByteArray();
+
+        final ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        final Unpacker unpacker = msgpack.createUnpacker(in);
+
+        ConsoleScriptResponseMessage inMsg;
+        try {
+            inMsg = unpacker.read(ConsoleScriptResponseMessage.class);
+            Assert.assertEquals(outMsg.Meta, inMsg.Meta);
+            Assert.assertEquals(UUID.nameUUIDFromBytes(outMsg.Request), UUID.nameUUIDFromBytes(inMsg.Request));
+            Assert.assertEquals(UUID.nameUUIDFromBytes(outMsg.Session), UUID.nameUUIDFromBytes(inMsg.Session));
+            Assert.assertTrue(Arrays.deepEquals(outMsg.ConsoleLines, inMsg.ConsoleLines));
+            Assert.assertEquals(outMsg.Bindings[0], inMsg.Bindings[0]);
+        } catch (IOException ex) {
+            Assert.fail();
+        }
     }
 }
--- rexster-protocol/src/test/java/com/tinkerpop/rexster/protocol/msg/ErrorResponseMessageTest.java
+++ rexster-protocol/src/test/java/com/tinkerpop/rexster/protocol/msg/ErrorResponseMessageTest.java
@@ -1,7 +1,16 @@
 package com.tinkerpop.rexster.protocol.msg;
 
+import com.tinkerpop.rexster.client.RexProException;
 import junit.framework.Assert;
 import org.junit.Test;
+import org.msgpack.MessagePack;
+import org.msgpack.packer.Packer;
+import org.msgpack.unpacker.Unpacker;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 /**
  * @author Stephen Mallette (http://stephen.genoprime.com)
@@ -14,5 +23,91 @@ public class ErrorResponseMessageTest {
         msg.ErrorMessage = "this was an error";
 
         Assert.assertEquals(53, msg.estimateMessageSize());
+    }
+
+    @Test
+    public void testMetaValidation() {
+        ErrorResponseMessage msg = new ErrorResponseMessage();
+        msg.Version = 0;
+        msg.setRequestAsUUID(UUID.randomUUID());
+        msg.setSessionAsUUID(UUID.randomUUID());
+        msg.metaSetFlag(ErrorResponseMessage.INVALID_SESSION_ERROR);
+        msg.ErrorMessage = "brokenness";
+
+        //these should work
+        try {
+            msg.metaSetFlag(ErrorResponseMessage.INVALID_MESSAGE_ERROR);
+            msg.validateMetaData();
+
+            msg.metaSetFlag(ErrorResponseMessage.INVALID_SESSION_ERROR);
+            msg.validateMetaData();
+
+            msg.metaSetFlag(ErrorResponseMessage.SCRIPT_FAILURE_ERROR);
+            msg.validateMetaData();
+
+            msg.metaSetFlag(ErrorResponseMessage.AUTH_FAILURE_ERROR);
+            msg.validateMetaData();
+
+        } catch (RexProException ex) {
+            Assert.fail();
+        }
+
+        //these should fail
+        try {
+            msg.Meta.put(ErrorResponseMessage.FLAG_META_KEY, "session");
+            msg.validateMetaData();
+        } catch (RexProException ex) {
+            //exception is expected
+        }
+        try {
+            msg.Meta.put(ErrorResponseMessage.FLAG_META_KEY, false);
+            msg.validateMetaData();
+        } catch (RexProException ex) {
+            //exception is expected
+        }
+        try {
+            msg.Meta.put(ErrorResponseMessage.FLAG_META_KEY, 3.14f);
+            msg.validateMetaData();
+        } catch (RexProException ex) {
+            //exception is expected
+        }
+    }
+
+    @Test
+    public void testSerialization() {
+        MessagePack msgpack = new MessagePack();
+        msgpack.register(RexProMessageMeta.class, RexProMessageMeta.SerializationTemplate.getInstance());
+
+        ErrorResponseMessage outMsg = new ErrorResponseMessage();
+        outMsg.Version = 0;
+        outMsg.setRequestAsUUID(UUID.randomUUID());
+        outMsg.setSessionAsUUID(UUID.randomUUID());
+        outMsg.metaSetFlag(ErrorResponseMessage.INVALID_SESSION_ERROR);
+        outMsg.ErrorMessage = "brokenness";
+
+        final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        final Packer packer = msgpack.createPacker(outStream);
+        try {
+            packer.write(outMsg);
+            packer.close();
+        } catch (IOException ex) {
+            Assert.fail();
+        }
+
+        byte[] bytes = outStream.toByteArray();
+
+        final ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        final Unpacker unpacker = msgpack.createUnpacker(in);
+
+        ErrorResponseMessage inMsg;
+        try {
+            inMsg = unpacker.read(ErrorResponseMessage.class);
+            Assert.assertEquals(outMsg.Meta, inMsg.Meta);
+            Assert.assertEquals(UUID.nameUUIDFromBytes(outMsg.Request), UUID.nameUUIDFromBytes(inMsg.Request));
+            Assert.assertEquals(UUID.nameUUIDFromBytes(outMsg.Session), UUID.nameUUIDFromBytes(inMsg.Session));
+            Assert.assertEquals(outMsg.ErrorMessage, inMsg.ErrorMessage);
+        } catch (IOException ex) {
+            Assert.fail();
+        }
     }
 }
