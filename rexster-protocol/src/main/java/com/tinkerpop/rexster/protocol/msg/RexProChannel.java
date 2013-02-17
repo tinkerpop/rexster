dd81ea7b843fb8c55a629ba020d21d1801703fd8
Stephen Mallette <spmva@genoprime.com>
2013-02-17 09:45:08 -0500

    Forgot to add important file to last commit.

---
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/msg/RexProChannel.java
@@ -0,0 +1,11 @@
+package com.tinkerpop.rexster.protocol.msg;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public final class RexProChannel {
+    public static final int CHANNEL_NONE = 0;
+    public static final int CHANNEL_CONSOLE = 1;
+    public static final int CHANNEL_MSGPACK = 2;
+    public static final int CHANNEL_GRAPHSON = 3;
+}
