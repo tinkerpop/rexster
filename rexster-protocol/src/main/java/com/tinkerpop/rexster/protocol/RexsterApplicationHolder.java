5e589e436a22a8e0002b8c4d4adf7752b950f6ba
Stephen Mallette <spmva@genoprime.com>
2013-02-21 21:17:31 -0500

    Forgot to add file.

---
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/RexsterApplicationHolder.java
@@ -0,0 +1,37 @@
+package com.tinkerpop.rexster.protocol;
+
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.rexster.RexsterApplicationGraph;
+import com.tinkerpop.rexster.Tokens;
+import com.tinkerpop.rexster.server.RexsterApplication;
+
+import java.util.Set;
+
+/**
+ * Proxies call to the RexsterApplication instance to limit the methods available in RexsterConsole.
+ *
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+public class RexsterApplicationHolder {
+    private final RexsterApplication rexsterApplication;
+
+    public RexsterApplicationHolder(final RexsterApplication rexsterApplication) {
+        this.rexsterApplication = rexsterApplication;
+    }
+
+    public Graph getGraph(final String graphName) {
+        return this.rexsterApplication.getGraph(graphName);
+    }
+
+    public RexsterApplicationGraph getApplicationGraph(final String graphName) {
+        return this.rexsterApplication.getApplicationGraph(graphName);
+    }
+
+    public Set<String> getGraphNames() {
+        return this.rexsterApplication.getGraphNames();
+    }
+
+    public String getVersion() {
+        return Tokens.REXSTER_VERSION;
+    }
+}
