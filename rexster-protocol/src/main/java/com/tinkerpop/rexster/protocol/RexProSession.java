ec7f4815cc5d175ad3bf113266c24046b17ee3c5
Blake Eggleston <bdeggleston@gmail.com>
2013-05-26 08:40:25 -0700

    cleaning up some of the filters

--- rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/RexProSession.java
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/RexProSession.java
@@ -16,7 +16,6 @@ import java.util.Date;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 /**
  * Server-side rexster session.  All requests to a session are bound to a specific thread.
