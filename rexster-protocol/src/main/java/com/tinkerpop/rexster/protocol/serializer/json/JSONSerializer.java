2b85d8369de3f22fea1cdd29248079d31313291b
Blake Eggleston <bdeggleston@gmail.com>
2013-07-07 21:13:22 -0700

    implementing the json serializer

--- rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/serializer/json/JSONSerializer.java
+++ rexster-protocol/src/main/java/com/tinkerpop/rexster/protocol/serializer/json/JSONSerializer.java
@@ -1,17 +1,51 @@
 package com.tinkerpop.rexster.protocol.serializer.json;
 
-import com.tinkerpop.rexster.protocol.msg.RexProMessage;
+import com.tinkerpop.rexster.protocol.msg.*;
 import com.tinkerpop.rexster.protocol.serializer.RexProSerializer;
+import com.tinkerpop.rexster.protocol.serializer.json.templates.*;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import java.io.IOException;
 
 public class JSONSerializer implements RexProSerializer {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
     public <Message extends RexProMessage> Message deserialize(byte[] bytes, Class<Message> messageClass) throws IOException {
-        return null;
+
+        RexProMessageTemplate template = null;
+        if (messageClass == ErrorResponseMessage.class) {
+            template = ErrorResponseMessageTemplate.getInstance();
+        } else if (messageClass == ScriptRequestMessage.class) {
+            template = ScriptRequestMessageTemplate.getInstance();
+        } else if (messageClass == ScriptResponseMessage.class) {
+            template = ScriptResponseMessageTemplate.getInstance();
+        } else if (messageClass == SessionRequestMessage.class) {
+            template = SessionRequestMessageTemplate.getInstance();
+        } else if (messageClass == SessionResponseMessage.class) {
+            template = SessionResponseMessageTemplate.getInstance();
+        }
+
+        return (Message) template.deserialize(mapper.readTree(bytes));
     }
 
     public <Message extends RexProMessage> byte[] serialize(Message message, Class<Message> messageClass) throws IOException {
-        return null;
+
+        RexProMessageTemplate template = null;
+        if (messageClass == ErrorResponseMessage.class) {
+            template = ErrorResponseMessageTemplate.getInstance();
+        } else if (messageClass == ScriptRequestMessage.class) {
+            template = ScriptRequestMessageTemplate.getInstance();
+        } else if (messageClass == ScriptResponseMessage.class) {
+            template = ScriptResponseMessageTemplate.getInstance();
+        } else if (messageClass == SessionRequestMessage.class) {
+            template = SessionRequestMessageTemplate.getInstance();
+        } else if (messageClass == SessionResponseMessage.class) {
+            template = SessionResponseMessageTemplate.getInstance();
+        }
+        JsonNode serialized = template.serialize(message);
+        return mapper.writeValueAsBytes(serialized);
     }
 
     @Override
