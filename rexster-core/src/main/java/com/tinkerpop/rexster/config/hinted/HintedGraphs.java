23b37a147ba482df649e3552df5da5b4be374298
Dan LaRocque <dalaro@hopcount.org>
2013-12-10 07:46:33 -0500

    Add toString() to HintedGraphs

--- rexster-core/src/main/java/com/tinkerpop/rexster/config/hinted/HintedGraphs.java
+++ rexster-core/src/main/java/com/tinkerpop/rexster/config/hinted/HintedGraphs.java
@@ -9,4 +9,31 @@ import java.util.Map;
  */
 public class HintedGraphs implements Serializable {
     public final Map<String,HintedGraph> graphs = new HashMap<String, HintedGraph>();
+
+    /*
+     * Guava's helpers could make this simpler
+     */
+    @Override
+    public String toString() {
+        final String g;
+
+        if (null == graphs) {
+            g = "null";
+        } else {
+            StringBuilder sb = new StringBuilder();
+            boolean tail = false;
+            for (Map.Entry<String, HintedGraph> ent : graphs.entrySet()) {
+                if (tail)
+                    sb.append(", ");
+
+                sb.append(ent.getKey());
+                sb.append("=");
+                sb.append(ent.getValue());
+                tail = true;
+            }
+            g = sb.toString();
+        }
+
+        return "HintedGraphs[" + g + "]";
+    }
 }
